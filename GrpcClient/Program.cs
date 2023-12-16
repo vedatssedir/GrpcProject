e2d4915fb8123a7a6580b63d438c34053cf69e25
Vedat Sedir <extvedats@ext.d-teknoloji.com.tr>
2023-12-16 17:02:17 +0300

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,61 @@
+﻿using Grpc.Net.Client;
+using grpcMessageServer;
+using GrpcServer;
+
+namespace GrpcClient;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var channel = GrpcChannel.ForAddress(new Uri("https://localhost:7265"));
+
+
+        //var greetClient = new Greeter.GreeterClient(channel);
+
+        //var result = await greetClient.SayHelloAsync(new HelloRequest()
+        //{
+        //    Name = "Vedat Sedir"
+        //});
+
+        //var messageClient = new Message.MessageClient(channel);
+        //var messageResponse = messageClient.Send(new MessageRequest() { Message = "test", Name = "vedat" });
+        //var cancellationTokenSource = new CancellationTokenSource();
+
+        //while (await messageResponse.ResponseStream.MoveNext(cancellationTokenSource.Token))
+        //{
+        //    Console.WriteLine(messageResponse.ResponseStream.Current);
+        //}
+
+
+        //Server Streaming
+        //var messageClient = new Message.MessageClient(channel);
+
+        //var message = await messageClient.Send(new MessageRequest()
+        //{
+        //    Name = "Vedat Sedir",
+        //    Message = "Bu test amaclıdır"
+        //});
+        //Console.WriteLine($"{result.Message} : {message.Message}");
+        //Console.ReadLine();
+
+        //Client Streaming
+        var messageClient = new Message.MessageClient(channel);
+        var request = messageClient.Send();
+
+        for (int i = 0; i < 10; i++)
+        {
+            await request.RequestStream.WriteAsync(new MessageRequest() { Message = $"Mesaj {i}", Name = "Vedat Sedir" });
+        }
+        await request.RequestStream.CompleteAsync();
+        Console.WriteLine((await request.ResponseAsync).Message);
+
+
+
+        Console.ReadLine();
+    }
+
+
+
+
+}
---
+++ GrpcServer/Services/MessageService.cs
@@ -0,0 +1,62 @@
+﻿using Grpc.Core;
+using grpcMessageServer;
+
+namespace GrpcServer.Services
+{
+    public class MessageService : Message.MessageBase
+    {
+        private readonly ILogger<MessageService> _logger;
+        public MessageService(ILogger<MessageService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task<MessageResponse> Send(IAsyncStreamReader<MessageRequest> requestStream, ServerCallContext context)
+        {
+
+            while (await requestStream.MoveNext(context.CancellationToken))
+            {
+                Console.WriteLine($"Message : {requestStream.Current.Message} | Name : {requestStream.Current.Name}");
+            }
+            return new MessageResponse()
+            {
+                Message = "Veri alınmıştır..."
+            };
+        }
+
+
+        //public override async Task Send(MessageRequest request, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+        //{
+        //    Console.WriteLine($"Message : {request.Message} Name: {request.Name}");
+        //    for (var i = 0; i < 100000; i++)
+        //    {
+        //        await responseStream.WriteAsync(new MessageResponse() { Message = $"Item : {i}", Name = "Vedat Sedir" });
+        //    }
+        //}
+
+
+
+
+        //public override Task Send(MessageRequest request, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+        //{
+
+
+        //    for (var i = 0; i < 10; i++)
+        //    {
+        //        Task.Delay(200);
+        //        responseStream.WriteAsync(new MessageResponse()
+        //        {
+        //            Name = "Vedat Sedir",
+        //            Message = "Test amaclıdır"
+        //        });
+        //    }
+
+
+        //    return responseStream.WriteAsync(new MessageResponse() );
+
+        //}
+
+
+
+    }
+}
